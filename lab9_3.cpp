2569462efc3667211a443c2b3614ce2e86828887
Uni <ni.nitid@gmial.com>
2022-12-23 16:11:17 +0700

    lab9_3 completed.

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,49 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+   
+int main(){
+    int age, Height, money;
+    string sum;
+    cout << "Enter your age: ";
+    cin >> age;
+    
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> Height;
+
+            if(Height < 100){
+                sum = "Chopper";
+
+            }else if(Height < 180){
+                sum = "Usopp";
+            }else{
+                cout << "Enter your bounty: ";
+                cin >> money;
+
+                if(money > 1100000000){
+                    sum = "Zoro";
+                }else{
+                    sum = "Sanji";
+                }
+            }
+    }else if(age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> money;
+
+        if(money > 500000000){
+            sum = "Jinbe";
+        }else{
+            sum = "Franky";
+        }
+
+    }else{
+         sum = "Brook";
+    }
+
+
+cout << "Your character = " << sum;
+}
+
+
